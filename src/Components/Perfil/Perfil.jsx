4dc838c0f6cff8b3dc95c0808ef0632da82ede0a
Samuel-prata <samuel.silverio001@hotmail.com>
2024-04-22 18:58:37 -0300

    feat: Operation Area

--- src/Components/Perfil/Perfil.jsx
+++ src/Components/Perfil/Perfil.jsx
@@ -8,46 +8,70 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { ContainerOperation, ContainerPerfil } from "./PerfilStyle";
+import PaperOperation from "./PaperOperation";
+import PaperOperationTwo from "./PixArea";
 
 export default function Perfil() {
   const [user, setUser] = useState([]);
 
   useEffect(() => {
     api
-      .get("/user/6")
+      .get("/user/7")
       .then((response) => setUser(response.data))
       .catch((err) => {
-        alert("Error to catch data from user");
+        // alert("Error to catch data from user");
       });
   }, []);
 
   return (
     <>
+      <ContainerPerfil>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            sx={{ height: 140 }}
+            image={
+              user?.photo != null
+                ? user?.photo
+                : "https://www.shutterstock.com/shutterstock/photos/2059817444/display_1500/stock-vector-no-image-available-photo-coming-soon-illustration-vector-2059817444.jpg"
+            }
+            title="Photo User"
+          />
+          <CardContent
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography gutterBottom variant="h5" component="div">
+              {user?.name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {user?.email}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="green"
+              style={{ fontSize: "larger", fontWeight: "bolder" }}
+            >
+              R$ {user?.amount}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Depositar</Button>
+            <Button size="small">Sacar</Button>
+            <Button size="small">Pagar</Button>
+            <Button size="small">Tranferir</Button>
+          </CardActions>
+        </Card>
 
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          sx={{ height: 140 }}
-          image={user?.photo != null ? user?.photo: 'https://www.shutterstock.com/shutterstock/photos/2059817444/display_1500/stock-vector-no-image-available-photo-coming-soon-illustration-vector-2059817444.jpg'}
-          title="Photo User"
-        />
-        <CardContent style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
-          <Typography gutterBottom variant="h5" component="div">
-            {user?.name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {user?.email}
-          </Typography>
-          <Typography variant="body2" color="green" style={{fontSize:"larger", fontWeight:'bolder'}}>
-           R$ {user?.amount}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Depositar</Button>
-          <Button size="small">Sacar</Button>
-          <Button size="small">Pagar</Button>
-          <Button size="small">Tranferir</Button>
-        </CardActions>
-      </Card>
+        <ContainerOperation>
+            <PaperOperation>
+
+            </PaperOperation>
+        </ContainerOperation>
+      </ContainerPerfil>
     </>
   );
 }
--- src/Components/Perfil/PerfilStyle.js
+++ src/Components/Perfil/PerfilStyle.js
@@ -1,2 +1,23 @@
 import styled from "styled-components";
 
+export const ContainerPerfil = styled.section`
+  height: 80vh;
+  background-color: black;
+  display: flex;
+`;
+
+export const ContainerOperation = styled.div`
+  background-color: orange;
+  height: 100%;
+  width: 80%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: column;
+`;
+
+export const Itens = styled.h3`
+color: black;
+font-size: 20px;
+
+`
